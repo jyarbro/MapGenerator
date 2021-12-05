254434fb8a40244cff41d739cc3950df0574ef5b
Jim Yarbro <yarbro@outlook.com>
2021-12-05 10:29:48 -0500

    cleanup boilerplate

--- Client/MainPage.xaml.cs
+++ Client/MainPage.xaml.cs
@@ -13,16 +13,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Nrrdio.MapGenerator.Client {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page {
         public MainPage() {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
