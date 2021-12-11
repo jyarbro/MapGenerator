90c90dd482251e1d7b253cad8dc825b11b1bd283
Jim Yarbro <yarbro@outlook.com>
2021-12-10 19:57:17 -0500

    Added canvas, generator, and visualizer from old project.

---
+++ Services/Generator.cs
@@ -0,0 +1,128 @@
+﻿using Nrrdio.Utilities.Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nrrdio.MapGenerator.Services {
+    public class Generator {
+        public Polygon Border { get; set; }
+
+        public Generator(Polygon border) {
+            if (!border.IsConvex()) {
+                throw new ArgumentException("Border polygon must be convex because I'm too lazy to program around complex polys.");
+            }
+
+            Border = border;
+        }
+
+        public List<Point> Points(int amount, int seed) {
+            var random = new Random(seed);
+            var points = new List<Point>();
+
+            points.AddRange(Border.Vertices);
+
+            double pointX;
+            double pointY;
+
+            var minX = Border.Vertices.Min(o => o.X);
+            var minY = Border.Vertices.Min(o => o.Y);
+            var maxX = Border.Vertices.Max(o => o.X);
+            var maxY = Border.Vertices.Max(o => o.Y);
+
+            var rangeX = maxX - minX;
+            var rangeY = maxY - minY;
+
+            var i = 0;
+
+            while (i < amount) {
+                pointX = minX + random.NextDouble() * rangeX;
+                pointY = minY + random.NextDouble() * rangeY;
+
+                var point = new Point(pointX, pointY);
+
+                if (Border.Contains(point)) {
+                    points.Add(point);
+                    i++;
+                }
+            }
+
+            return points;
+        }
+
+        public IEnumerable<Polygon> DelaunayTriangles(List<Point> points) {
+            var borderVertices = Border.Vertices.Count;
+            int j;
+
+            var triangles = new HashSet<Polygon> { Border };
+
+            for (var i = 0; i < borderVertices; i++) {
+                j = (i + 1) % borderVertices;
+
+                var triangle = new Polygon(Border.Centroid, Border.Vertices[i], Border.Vertices[j]);
+                triangles.Add(triangle);
+            }
+
+            foreach (var point in points) {
+                var badTriangles = findBadTriangles(point, triangles);
+                var polygon = findHoleBoundaries(badTriangles);
+
+                foreach (var badTriangle in badTriangles) {
+                    foreach (var vertex in badTriangle.Vertices) {
+                        vertex.AdjacentPolygons.Remove(badTriangle);
+                    }
+                }
+
+                triangles.RemoveWhere(badTriangles.Contains);
+
+                foreach (var edge in polygon.Where(possibleEdge => possibleEdge.Point1 != point && possibleEdge.Point2 != point)) {
+                    var triangle = new Polygon(point, edge.Point1, edge.Point2);
+                    triangles.Add(triangle);
+                }
+            }
+
+            return triangles;
+
+            ISet<Polygon> findBadTriangles(Point target, ISet<Polygon> potentials) {
+                var badTriangles = potentials.Where(o => o.Circumcircle.Contains(target));
+                return new HashSet<Polygon>(badTriangles);
+            }
+
+            IEnumerable<Segment> findHoleBoundaries(ISet<Polygon> badTriangles) {
+                var edges = new List<Segment>();
+
+                foreach (var triangle in badTriangles) {
+                    edges.Add(new Segment(triangle.Vertices[0], triangle.Vertices[1]));
+                    edges.Add(new Segment(triangle.Vertices[1], triangle.Vertices[2]));
+                    edges.Add(new Segment(triangle.Vertices[2], triangle.Vertices[0]));
+                }
+
+                return edges.GroupBy(o => o).Where(o => o.Count() == 1).Select(o => o.First());
+            }
+        }
+
+        public IEnumerable<Segment> TriangleEdges(IEnumerable<Polygon> triangles) {
+            var edges = new HashSet<Segment>();
+
+            foreach (var triangle in triangles) {
+                edges.Add(new Segment(triangle.Vertices[0], triangle.Vertices[1]));
+                edges.Add(new Segment(triangle.Vertices[1], triangle.Vertices[2]));
+                edges.Add(new Segment(triangle.Vertices[2], triangle.Vertices[0]));
+            }
+
+            return edges;
+        }
+
+        public IEnumerable<Segment> VoronoiEdges(IEnumerable<Polygon> triangles) {
+            var edges = new HashSet<Segment>();
+
+            foreach (var triangle in triangles) {
+                foreach (var neighbor in triangle.Neighbors) {
+                    var edge = new Segment(triangle.Circumcircle.Center, neighbor.Circumcircle.Center);
+                    edges.Add(edge);
+                }
+            }
+
+            return edges;
+        }
+    }
+}
--- Services/MainPageViewModel.cs
+++ Services/MainPageViewModel.cs
@@ -1,18 +1,149 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Nrrdio.Utilities.Maths;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Nrrdio.MapGenerator.Services {
     public class MainPageViewModel {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public int Seed {
+            get => seed;
+            set {
+                if (seed != value) {
+                    seed = value;
+                    OnPropertyChanged(nameof(Seed));
+                }
+            }
+        }
+        int seed;
+
+        public int PointCount {
+            get => pointCount;
+            set {
+                if (pointCount != value) {
+                    pointCount = value;
+                    OnPropertyChanged(nameof(PointCount));
+                }
+            }
+        }
+        int pointCount = 2000;
+
+        public Canvas OutputCanvas { get; set; }
+
+        public int CanvasWidth {
+            get => canvasWidth;
+            set {
+                if (canvasWidth != value) {
+                    canvasWidth = value;
+                    OnPropertyChanged(nameof(CanvasWidth));
+                }
+            }
+        }
+        int canvasWidth;
+
+        public int CanvasHeight {
+            get => canvasHeight;
+            set {
+                if (canvasHeight != value) {
+                    canvasHeight = value;
+                    OnPropertyChanged(nameof(CanvasHeight));
+                }
+            }
+        }
+        int canvasHeight;
+
         ILogger<MainPageViewModel> Log { get; }
+        Generator Generator { get; set; }
+        Visualizer Visualizer { get; set; }
+        Task SizeChangedTask { get; set; }
+        bool Waiting { get; set; }
 
         public MainPageViewModel() {
             Log = Ioc.Default.GetService<ILogger<MainPageViewModel>>();
         }
 
-        public void LogTest() {
-            Log.LogInformation("test");
-            Log.LogError("error!!");
-            Log.LogWarning("warnung");
+        public void GenerateAndDraw() {
+            OutputCanvas.Children.Clear();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var points = Generator.Points(PointCount, Seed);
+            var triangles = Generator.DelaunayTriangles(points);
+            var triangleEdges = Generator.TriangleEdges(triangles);
+            var voronoiEdges = Generator.VoronoiEdges(triangles);
+
+            stopwatch.Stop();
+            Log.LogInformation($"Generator duration: {stopwatch.ElapsedMilliseconds / 1000f} sec");
+            stopwatch.Restart();
+
+            var trianglePath = Visualizer.GetPath(triangleEdges, 1, Colors.LightSteelBlue);
+            var voronoiPath = Visualizer.GetPath(voronoiEdges, 2, Colors.DarkViolet);
+            var pointShapes = Visualizer.GetPointShapes(points, 3, Colors.Red);
+
+            stopwatch.Stop();
+            Log.LogInformation($"Visualizer duration: {stopwatch.ElapsedMilliseconds / 1000f} sec");
+            stopwatch.Restart();
+
+            foreach (var pointShape in pointShapes) {
+                OutputCanvas.Children.Add(pointShape);
+            }
+
+            OutputCanvas.Children.Add(trianglePath);
+            //OutputCanvas.Children.Add(voronoiPath);
+
+            stopwatch.Stop();
+            Log.LogInformation($"Canvas duration: {stopwatch.ElapsedMilliseconds / 1000f} sec");
+        }
+
+        public async Task UpdateCanvasSize() {
+            await Task.Run(() => {
+                while (Waiting) {
+                    Waiting = false;
+                    Thread.Sleep(250);
+                }
+            });
+
+            CanvasHeight = (int)OutputCanvas.ActualHeight;
+            CanvasWidth = (int)OutputCanvas.ActualWidth;
+        }
+
+        public void OnDrawButtonClick(object sender, RoutedEventArgs e) {
+            GenerateAndDraw();
+        }
+
+        public void OnWindowSizeChanged(object sender, SizeChangedEventArgs e) {
+            Waiting = true;
+
+            if (SizeChangedTask is null || SizeChangedTask.IsCompleted) {
+                SizeChangedTask = UpdateCanvasSize();
+            }
+        }
+
+        public void OnWindowLoaded(object sender, RoutedEventArgs e) {
+            Seed = new Random().Next();
+
+            OnPropertyChanged(nameof(Seed));
+
+            CanvasHeight = (int)OutputCanvas.ActualHeight;
+            CanvasWidth = (int)OutputCanvas.ActualWidth;
+
+            var canvasArea = new Polygon(new Point(0, 0), new Point(0, CanvasHeight), new Point(CanvasWidth, CanvasHeight), new Point(CanvasWidth, 0));
+
+            Generator = new Generator(canvasArea);
+            Visualizer = new Visualizer();
+        }
+
+        public void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
