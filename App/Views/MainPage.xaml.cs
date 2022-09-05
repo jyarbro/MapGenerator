be0a19fc2733f274cd15979ab70be547886d61c4
Jim Yarbro <yarbro@outlook.com>
2022-09-05 09:58:25 -0400

    Add WinUI3 app from template

---
+++ App/Views/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App.ViewModels;
+
+using CommunityToolkit.WinUI.UI.Controls;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace App.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+
+    private void OnViewStateChanged(object sender, ListDetailsViewState e)
+    {
+        if (e == ListDetailsViewState.Both)
+        {
+            ViewModel.EnsureItemSelected();
+        }
+    }
+}
