56f67a3fd1d8e66acf986493fe062a661c2bf8f0
Jim Yarbro <yarbro@outlook.com>
2023-06-26 15:25:11 -0400

    Removed old client.

--- ClientOld/MainWindow.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.UI.Xaml;
-
-namespace Nrrdio.MapGenerator.Client {
-    public sealed partial class MainWindow : Window {
-        public MainWindow() => InitializeComponent();
-    }
-}
